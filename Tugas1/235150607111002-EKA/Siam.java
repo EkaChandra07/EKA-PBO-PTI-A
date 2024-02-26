574cdd72b4aa8bd1f760dab0e4f8e62a7e0cce23
EkaChandra07 <160575743+EkaChandra07@users.noreply.github.com>
2024-02-26 12:02:58 +0700

    Update Siam.java

--- Tugas1/235150607111002-EKA/Siam.java
+++ Tugas1/235150607111002-EKA/Siam.java
@@ -4,31 +4,40 @@ public class Siam {
     public static void main(String[] args) {
         String nim = "235150607111002";
         String password = "ekonihboss";
-        boolean isLoggedIn = false;
+        boolean sudahLogin = false;
+
+        String nama = "Eka Chandra Winata";
+        int umur = 20;
+        String alamat = "Jalan Dr. Sutomo No.159";
 
         Scanner input = new Scanner(System.in);
 
         while (true) {
-            if (!isLoggedIn) {
+            if (!sudahLogin) {
                 System.out.println("Selamat datang di SIAM, Silakan login!");
                 System.out.print("Masukkan nim anda     : ");
-                String inputNim = input.nextLine();
+                String masukanNim = input.nextLine();
                 System.out.print("Masukkan password anda: ");
-                String inputPassword = input.nextLine();
+                String masukanPassword = input.nextLine();
 
-                if (nim.equals(inputNim) && password.equals(inputPassword)) {
-                    isLoggedIn = true;
+                if (nim.equals(masukanNim) && password.equals(masukanPassword)) {
+                    sudahLogin = true;
                     System.out.println("Anda berhasil login!");
                 } else {
                     System.out.println("Nim atau password anda salah. Silakan coba lagi.");
                 }
             } else {
                 System.out.println("Anda telah login.");
-                System.out.println("Apakah Anda ingin melakukan logout? (iya/tidak)");
-                String choice = input.nextLine().toLowerCase();
+                System.out.println("Apakah Anda ingin melihat biodata Eka? (iya/tidak/logout)");
+                String pilih = input.nextLine().toLowerCase();
 
-                if (choice.equals("iya")) {
-                    isLoggedIn = false;
+                if (pilih.equals("iya")) {
+                    System.out.println("Biodata Eka:");
+                    System.out.println("Nama: " + nama);
+                    System.out.println("Umur: " + umur);
+                    System.out.println("Alamat: " + alamat);
+                } else if (pilih.equals("logout")) {
+                    sudahLogin = false;
                     System.out.println("Anda berhasil logout.");
                     break;
                 } else {
